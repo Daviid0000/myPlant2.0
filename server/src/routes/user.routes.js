42e6e06af8ac560c125cf67c0b26192df16cda3a
David Liva <davidliva@gmail.com>
2025-03-30 19:08:02 -0300

    Implementaciones: middleware para verificar la existencia de errores en los formularios

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,11 +1,13 @@
 import Router from 'express';
 import { createUser, deleteUser, getOneUser, getUsers, updateUser, userLogin } from '../controllers/users.controller.js';
+import { errorValidation } from '../../middleware/error.validation.js';
+import { userSchema } from '../models/Schemas/user.schemas.js';
 
 export const userRoutes = Router();
 
-userRoutes.post('/', createUser);
+userRoutes.post('/', userSchema, errorValidation, createUser);
 userRoutes.get('/', getUsers);
 userRoutes.get('/:id', getOneUser);
 userRoutes.put('/:id', updateUser);
 userRoutes.delete('/:id', deleteUser);
-userRoutes.post('/login', userLogin);
+userRoutes.post('/login', userSchema, errorValidation, userLogin);
